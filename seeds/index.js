26064137e8f0981055dadf74cd4065e6aaadb47d
cmseibel <cam.m.seib@gmail.com>
2022-02-20 18:59:06 -0800

    Fix bulkCreate calls.

--- seeds/index.js
+++ seeds/index.js
@@ -1,12 +1,15 @@
+const sequelize = require("../config/connection");
 const {Comment, Post, User} = require("../models");
 const commentData = require("./commentData.json");
 const postData = require("./postData.json");
 const userData = require("./userData.json");
 
 async function seedDB(){
-    await User.bulkBuild(userData);
-    await Post.bulkBuild(postData);
-    await Comment.bulkBuild(commentData);
+    await sequelize.sync({force: true});
+
+    await User.bulkCreate(userData);
+    await Post.bulkCreate(postData);
+    await Comment.bulkCreate(commentData);
 }
 
 seedDB();
