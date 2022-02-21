d6656a888d675388b8ba47f468dcea4b5f7f0320
cmseibel <cam.m.seib@gmail.com>
2022-02-20 18:03:25 -0800

    Write Comment model.

---
+++ models/Comment.js
@@ -0,0 +1,47 @@
+const {Model, DataTypes} = require("sequelize");
+const sequelize = require("../config/");
+
+class Comment extends Model{}
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        posted: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "user",
+                key: "id"
+            }
+        },
+        post_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "post",
+                key: "id"
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: "comment",
+    }
+);
